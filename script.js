8c3ef3a77b25d74239b1fa50f524600b9668e8a2
Steve Alverson <alverson.s@yahoo.com>
2020-10-24 15:55:00 -0400

    Infinite scroll working, loader visible when page refresh

--- script.js
+++ script.js
@@ -66,6 +66,14 @@ async function getPhotos() {
   }
 }
 
+// Check to see if scrolling near bottom of page, Load More Photos
+window.addEventListener('scroll', () => {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+    ready = false;
+    getPhotos();
+  }
+});
+
 
 // On Load
 getPhotos();
